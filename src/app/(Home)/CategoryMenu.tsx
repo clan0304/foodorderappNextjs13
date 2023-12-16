ffa32d5153fd8e25ba54bff6a8fbee53f08936f5
clan0304 <clan0304@gmail.com>
2023-12-16 22:53:40 +1100

    changed

--- src/app/(Home)/CategoryMenu.tsx
+++ src/app/(Home)/CategoryMenu.tsx
@@ -16,6 +16,9 @@ export const getProducts = async () => {
   }
 };
 const CategoryMenu = async () => {
+  if (!BASE_API_URL) {
+    return null;
+  }
   const products: ProductType[] = await getProducts();
   const riceProducts: ProductType[] = products.filter(
     (product) => product.category === 'rice'
--- src/app/(Home)/PopularMenu.tsx
+++ src/app/(Home)/PopularMenu.tsx
@@ -18,6 +18,9 @@ export const getProducts = async () => {
 };
 
 const PopularMenu = async () => {
+  if (!BASE_API_URL) {
+    return null;
+  }
   const products: ProductType[] = await getProducts();
 
   const popularProducts: ProductType[] = products.filter(
