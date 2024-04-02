293a5cefcd84a88ddf48383f16d733cec2027027
clan0304 <clan0304@gmail.com>
2024-04-02 15:46:37 +1100

    changed

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -83,7 +83,7 @@ const Cart = () => {
               </div>
             </div>
             <div className="flex items-center flex-col sm:flex-row sm:gap-10">
-              <span className="text-blue-600 font-bold text-sm sm:text-md md:text-lg">
+              <span className="text-blue-600 font-bold text-xs sm:text-sm md:text-md">
                 $ {product.price}
               </span>
             </div>
